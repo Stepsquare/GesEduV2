582b61b757d480d6ec6b4edceda149785c915b8d
Luís Parreira <luis.parreira@squad.pt>
2024-07-09 16:56:34 +0100

    Implementação de Areas; Update do layout das views da aplicação.

---
+++ GesEdu/Areas/MEGA/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using GesEdu.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SmartBreadcrumbs.Attributes;
+
+namespace GesEdu.Areas.MEGA.Controllers
+{
+    [Area("MEGA")]
+    [Authorize(Policy = "MegaAccess")]
+    public class HomeController : BaseController
+    {
+        [Breadcrumb(FromController = typeof(GesEdu.Controllers.HomeController), FromAction = "Index", Title = "MEGA")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GesEdu/Areas/SIME/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using GesEdu.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SmartBreadcrumbs.Attributes;
+
+namespace GesEdu.Areas.SIME.Controllers
+{
+    [Area("SIME")]
+    [Authorize(Policy = "SimeAccess")]
+    public class HomeController : BaseController
+    {
+        [Breadcrumb(FromController = typeof(GesEdu.Controllers.HomeController), FromAction = "Index", Title = "SIME")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
