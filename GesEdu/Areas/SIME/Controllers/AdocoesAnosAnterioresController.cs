f575ec844477f2ec46e2cbec947f65fe75fac6e6
Luís Parreira <luis.parreira@squad.pt>
2025-05-27 11:06:23 +0100

    Adição views e controllers de páginas SIME. Correção breadcrumbs.

---
+++ GesEdu/Areas/SIME/Controllers/AdocoesAnosAnterioresController.cs
@@ -0,0 +1,18 @@
+﻿using GesEdu.Controllers;
+using GesEdu.Helpers.Breadcrumbs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GesEdu.Areas.SIME.Controllers
+{
+    [Area("SIME")]
+    [Authorize(Policy = "SimeAccess")]
+    public class AdocoesAnosAnterioresController : BaseController
+    {
+        [Breadcrumb(Title = "Adoções anos anteriores")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GesEdu/Areas/SIME/Controllers/PedidosExcecaoController.cs
@@ -0,0 +1,18 @@
+﻿using GesEdu.Controllers;
+using GesEdu.Helpers.Breadcrumbs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GesEdu.Areas.SIME.Controllers
+{
+    [Area("SIME")]
+    [Authorize(Policy = "SimeAccess")]
+    public class PedidosExcecaoController : BaseController
+    {
+        [Breadcrumb(Title = "Pedidos de exceção - Listagem")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
